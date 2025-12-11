6833b29bc15cd09bdfa4854c68e28ec0b39d14e2
Zdenek <63140432+linhartz@users.noreply.github.com>
2025-12-11 18:26:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import os
 
-app = FastAPI()
+app = FastAPI(title='DEAT ARC–NUR Stabilizer', version='0.2')
 
 # Mount static directory
 static_dir = os.path.join(os.path.dirname(__file__), "static")
-app.mount("/static", StaticFiles(directory=static_dir), name="static")
-
+app.mount('/static', StaticFiles(directory="static"), name="static")
 
 # Serve editor.html from static folder
 @app.get("/editor", include_in_schema=False)
